9f70ecd2add285df7813b89b933147e24ecafb5e
b_stef <stephane.betton@gmail.com>
2021-08-21 11:25:23 +0200

    init projet

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Front
+https://angular.io/tutorial/toh-pt5
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.7.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MembresListeComponent } from './membres/membres-liste/membres-liste.component';
+import {RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {ResponsiveToolbarComponent} from './layout/menu/responsive-toolbar/responsive-toolbar.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'membresnpm audit', component: MembresListeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MembresListeComponent,
+    ResponsiveToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
