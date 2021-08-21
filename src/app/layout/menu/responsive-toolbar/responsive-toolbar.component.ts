063270eeedca57b921b946eb710c6bc9c1fc28c6
b_stef <stephane.betton@gmail.com>
2021-08-21 11:26:17 +0200

    init projet

---
+++ src/app/layout/menu/responsive-toolbar/responsive-toolbar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from '../../../models/menu/menu-item';
+
+@Component({
+  selector: 'app-responsive-toolbar',
+  templateUrl: './responsive-toolbar.component.html',
+  styleUrls: ['./responsive-toolbar.component.css']
+})
+export class ResponsiveToolbarComponent implements OnInit {
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Sign Up',
+      icon: 'login'
+    },
+    {
+      label: 'About',
+      icon: 'help'
+    },
+    {
+      label: 'Pricing',
+      icon: 'attach_money'
+    },
+    {
+      label: 'Docs',
+      icon: 'notes'
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow'
+    },
+    {
+      label: 'Blog',
+      icon: 'rss_feed'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/membres/membres-liste/membres-liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-membres-liste',
+  templateUrl: './membres-liste.component.html',
+  styleUrls: ['./membres-liste.component.css']
+})
+export class MembresListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
